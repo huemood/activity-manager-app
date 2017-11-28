d76aa0b892dc4414ed143a1bb1bea20c279b4cc9
huemood <huemood@126.com>
2017-11-28 17:23:00 +0800

    cas client
    添加nodejs本地代理

--- config/index.js
+++ config/index.js
@@ -27,11 +27,19 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: process.env.PORT || 8080,
+    port: process.env.PORT || 8081,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/aapi': {
+        target: 'http://localhost:9001/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/aapi': '/'
+        }
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
